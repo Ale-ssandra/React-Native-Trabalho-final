b919e2cfb2ca1ae7238f21559ea49e3982fc802d
BilPinheiro <prod.gabriel.pinheiro@gmail.com>
2022-11-23 19:11:45 -0300

    Co-authored-by: Erick Viana <ErickNotFound@users.noreply.github.com> Co-authored-by: José Pedro Nolasco <J-Pedr0@users.noreply.github.com>

--- MobileEcommerce/src/routes/RotasPublicas.jsx
+++ MobileEcommerce/src/routes/RotasPublicas.jsx
@@ -4,9 +4,9 @@ import {Login} from '../pages/Login/index';
 import {Home} from '../pages/Home';
 import {Carrinho} from '../pages/Carrinho';
 import {Pesquisa} from '../pages/Pesquisa';
-import { Colaboradores } from '../pages/Colaboradores';
+import {Colaboradores} from '../pages/Colaboradores';
 import {StyleSheet, View, Text, Image} from 'react-native';
-import { Produto } from '../pages/produto';
+import {Produto} from '../pages/produto';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -21,109 +21,162 @@ export const RotasPublicas = () => {
             borderTopWidth: 0,
             position: 'absolute',
             height: 60,
-            backgroundColor:'#272626',
-            borderRadius:20,
+            backgroundColor: '#272626',
+            borderRadius: 20,
             left: 5,
             right: 5,
             bottom: 5,
-            elevation:0,
-            ...styles.shadow
-          }
-        ]
+            elevation: 0,
+            ...styles.shadow,
+          },
+        ],
       }}>
-      <Tab.Screen 
-      name="Home" 
-      component={Produto} 
-      options={{headerShown: false,
-      tabBarIcon: ({focused}) =>
-        (
-          <View style={{alignItems: 'center', justifyContent: 'center'}}>
-            <Image source={require('../images/home.png')} resizeMode='contain' style={{
-              width: 30,
-              height: 30,
-              tintColor: focused ? '#B69DF8' : 'white',
-            }} />
-            <Text style={{color: focused ? '#B69DF8' : 'white', fontSize: 12, marginTop: 3}}>Home</Text>
-          </View>
-        )
-    }} 
-      
+      <Tab.Screen
+        name="Home"
+        component={Carrinho}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/home.png')}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? '#B69DF8' : 'white',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Home
+              </Text>
+            </View>
+          ),
+        }}
       />
       <Tab.Screen
         name="Pesquisa"
         component={Produto}
-        options={{headerShown: false,
-          tabBarIcon: ({focused}) =>
-            (
-              <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                <Image source={require('../images/search.png')} resizeMode='contain' style={{
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/search.png')}
+                resizeMode="contain"
+                style={{
                   width: 30,
                   height: 30,
                   tintColor: focused ? '#B69DF8' : 'white',
-                }} />
-                <Text style={{color: focused ? '#B69DF8' : 'white', fontSize: 12, marginTop: 3}}>Produto</Text>
-              </View>
-            )
-        }} 
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Produto
+              </Text>
+            </View>
+          ),
+        }}
       />
       <Tab.Screen
         name="Carrinho"
         component={Carrinho}
-        options={{headerShown: false,
-          tabBarIcon: ({focused}) =>
-            (
-              <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                <Image source={require('../images/carrinho.png')} resizeMode='contain' style={{
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/carrinho.png')}
+                resizeMode="contain"
+                style={{
                   width: 30,
                   height: 30,
                   tintColor: focused ? '#B69DF8' : 'white',
-                }} />
-                <Text style={{color: focused ? '#B69DF8' : 'white', fontSize: 12, marginTop: 3}}>Carrinho</Text>
-              </View>
-            )
-        }} 
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Carrinho
+              </Text>
+            </View>
+          ),
+        }}
       />
-      <Tab.Screen 
-      name="Colaboradores" 
-      component={Colaboradores} 
-      options={{headerShown: false,
-      tabBarIcon: ({focused}) =>
-        (
-          <View style={{alignItems: 'center', justifyContent: 'center'}}>
-            <Image source={require('../images/hacker.png')} resizeMode='contain' style={{
-              width: 30,
-              height: 30,
-              tintColor: focused ? '#B69DF8' : 'white',
-            }} />
-            <Text style={{color: focused ? '#B69DF8' : 'white', fontSize: 12, marginTop: 3}}>Staff</Text>
-          </View>
-        )
-    }} 
-      
+      <Tab.Screen
+        name="Colaboradores"
+        component={Colaboradores}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/hacker.png')}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? '#B69DF8' : 'white',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Staff
+              </Text>
+            </View>
+          ),
+        }}
       />
       <Tab.Screen
         name="Login"
         component={Login}
-        options={{headerShown: false,
-          tabBarIcon: ({focused}) =>
-            (
-              <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                <Image source={require('../images/user.png')} resizeMode='contain' style={{
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/user.png')}
+                resizeMode="contain"
+                style={{
                   width: 30,
                   height: 30,
                   tintColor: focused ? '#B69DF8' : 'white',
-                }} />
-                <Text style={{color: focused ? '#B69DF8' : 'white', fontSize: 12, marginTop: 3}}>Login</Text>
-              </View>
-            )
-        }} 
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Login
+              </Text>
+            </View>
+          ),
+        }}
       />
     </Tab.Navigator>
   );
 };
 
 const styles = StyleSheet.create({
-  shadow:{
+  shadow: {
     shadowColor: '#B69DF8',
     shadowOffset: {
       width: 5,
@@ -132,5 +185,5 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 5,
     elevation: 3,
-  }
+  },
 });
