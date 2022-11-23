a53f26c79b94433002efefb151a75a02f5cf48d2
BilPinheiro <prod.gabriel.pinheiro@gmail.com>
2022-11-22 23:20:36 -0300

    a

--- MobileEcommerce/src/pages/Login/index.jsx
+++ MobileEcommerce/src/pages/Login/index.jsx
@@ -1,12 +1,25 @@
-import { Text } from "react-native"
-import React from "react"
-export const Login =({navigation})=>{
-    return(
-    <>
-    <Text>oi</Text>
-    
-    </>
-    )
+import {Text, View, ImageBackground, Image, Button, TextInput} from 'react-native';
+import React from 'react';
+import {styles} from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-}
+export const Login = ({navigation}) => {
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          imageStyle={{opacity: 0.08}}
+          source={require('../../images/texturaFundo.png')}
+          style={styles.imageBackground}>
+          <View style={styles.logo}>
+            <View style={styles.iconShadow}>
+              <Icon name="user-circle-o" size={200} color={'#B69DF8'} />
+            </View>
+          </View>
+          <TextInput
+              editable
+        maxLength={40}
+      />
+        </ImageBackground>
+      </View>
+    );
+  };
