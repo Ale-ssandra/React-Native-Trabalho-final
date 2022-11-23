8ba399abdb6179be24890f7e9c52954958b1bdb5
BilPinheiro <prod.gabriel.pinheiro@gmail.com>
2022-11-22 22:52:54 -0300

    Pagina Login, não finalizada

---
+++ MobileEcommerce/src/pages/Login/styles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1C1B1F',
+  },
+
+  imageBackground: {
+    flex: 1,
+  },
+
+  logo: {
+    height: '40%',
+    width:  '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  inputs: {
+    height: '60%',
+},
+
+
+});
