aa74c81184077c5147b7f81167e19d93f8b9ddf1
BilPinheiro <prod.gabriel.pinheiro@gmail.com>
2022-11-24 20:27:37 -0300

    Co-authored-by: José Pedro Nolasco <J-Pedr0@users.noreply.github.com> Co-authored-by: Erick Viana <ErickNotFound@users.noreply.github.com>

--- MobileEcommerce/src/routes/RotasPrivadas.jsx
+++ MobileEcommerce/src/routes/RotasPrivadas.jsx
@@ -1,30 +1,180 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { Home } from './src/pages/home';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home} from '../pages/Home';
+import {Carrinho} from '../pages/Carrinho';
+import {Pesquisa} from '../pages/Pesquisa';
+import {StyleSheet, View, Text, Image} from 'react-native';
+import {Produto} from '../pages/produto';
+import {Usuario} from '../pages/Usuario';
 
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator()
-const RotasPrivadas = () => {
 
-return(
-    <NavigationContainer>
-    <Tab.Navigator>
-    <Tab.Screen name="Home" component={Home}/>
-    <Tab.Screen name="Carrinho" component={Carrinho}/>
-    <Tab.Screen name="Login" component={Login}/>
-    <Tab.Screen name="Produto" component={Produto}/>
+export const HomeStack = () => {
+ return( <Stack.Navigator >
+    <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+
+    <Stack.Screen name="Produto" component={Produto} options={{ title: "Nome Produto", headerShown: true}} />
+  </Stack.Navigator>
+ );
+}
+export const SearchStack = () => {
+ return( <Stack.Navigator >
+    <Stack.Screen name="Pesquisa" component={Pesquisa} options={{headerShown: false}} />
+
+    <Stack.Screen name="Produto" component={Produto} options={{ title: "Nome Produto", headerShown: true}} />
+  </Stack.Navigator>
+ );
+}
+
+
+
+
+export const RotasPrivadas = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: [
+          {
+            borderTopWidth: 0,
+            position: 'absolute',
+            height: 60,
+            backgroundColor: '#272626',
+            borderRadius: 20,
+            left: 5,
+            right: 5,
+            bottom: 5,
+            elevation: 0,
+            ...styles.shadow,
+          },
+        ],
+      }}>
+      <Tab.Screen
+        name="HomeStack"
+        component={HomeStack}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/home.png')}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? '#B69DF8' : 'white',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Home
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="PesquisaStack"
+        component={SearchStack}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/search.png')}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? '#B69DF8' : 'white',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Pesquisa
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Carrinho"
+        component={Carrinho}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/carrinho.png')}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? '#B69DF8' : 'white',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Carrinho
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Usuario"
+        component={Usuario}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Image
+                source={require('../images/user.png')}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? '#B69DF8' : 'white',
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? '#B69DF8' : 'white',
+                  fontSize: 12,
+                  marginTop: 3,
+                }}>
+                Usuario
+              </Text>
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
+  );
+};
 
-    <Stack.Navigator>
-    
-    <Stack.Screen name="Carrinho" component={Carrinho}/>
-    <Stack.Screen name="Produto" component={Produto}/>
-
-    </Stack.Navigator>
-
-    </NavigationContainer>
-)
-
-} 
- export default RotasPrivadas
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#B69DF8',
+    shadowOffset: {
+      width: 5,
+      height: 0,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+});
